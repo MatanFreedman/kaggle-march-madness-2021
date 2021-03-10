8405dec1349e4e7368d4e1f0e1011339fdca2510
MatanFreedman <matan.freedman@gmail.com>
2021-03-10 17:51:40 -0500

    updated makefile.py, model building pipeline

--- makefile.py
+++ makefile.py
@@ -6,5 +6,10 @@ if __name__ == "__main__":
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.DEBUG, format=log_fmt)
 
+    # pulls kenpom data and saves in external:
+    from src.data import make_dataset
+    make_dataset.main()
+
+    # uses kaggle data in "raw" and kenpom in "external", cleans and builds features.
     from src.features import build_features
     build_features.main()
